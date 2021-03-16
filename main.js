5ccdb2c520d84f4ba45abdc73e99998d003601c1
nediakhelifi <khelifinedia91@gmail.com>
2021-03-16 13:40:42 +0100

    init

---
+++ main.js
@@ -0,0 +1,23 @@
+students=[];
+function newchild(firstname,lastname,age,parentphone){
+
+    return{
+        firstname:firstname,
+        lastname:lastname,
+        age:age,
+        parentphone:parentphone
+         };
+}
+   function addstudent(students,newstudent){
+    return students.push(newstudent)
+   }
+
+ 
+function addlike(){
+  var counter=0
+  return counter=counter+1}
+ 
+  function displaydrawer (photos){
+    return  'drawed by :'+student.firstname+''+student.lastname
+  }
+  
